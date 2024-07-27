60b7966419b184620fcb1db2a822c47c78072186
k-nakamura <koya.nakamura@gmail.com>
2024-07-27 20:58:59 +0900

    move markdown rendering to client side

--- src/components/sections/ConferenceSection.tsx
+++ src/components/sections/ConferenceSection.tsx
@@ -7,9 +7,14 @@ import Day1En from "@/components/markdown/keynote_day1_en.mdx";
 import Day2En from "@/components/markdown/keynote_day2_en.mdx";
 import InvitationTalkJa from "@/components/markdown/invitation_talk_ja.mdx";
 import InvitationTalkEn from "@/components/markdown/invitation_talk_en.mdx";
+import dynamic from "next/dynamic";
 
 export default async function ConferenceSection({lang}: { lang: 'ja' | 'en' }) {
   const dictionary = await getDictionary(lang)
+
+  const Keynote1 = dynamic(() => import('@/components/markdown/keynote_day1_' + lang + '.mdx'), {ssr: false});
+  const Keynote2 = dynamic(() => import('@/components/markdown/keynote_day2_' + lang + '.mdx'), {ssr: false});
+  const InvitationTalk = dynamic(() => import('@/components/markdown/invitation_talk_' + lang + '.mdx'), {ssr: false});
 
   return <section className="mt-20">
     <SectionTitle title={'Conference'} subtitle={dictionary.conference.subtitle} direction={'down'}
@@ -25,12 +30,12 @@ export default async function ConferenceSection({lang}: { lang: 'ja' | 'en' }) {
     <div className='flex lg:gap-7 gap-8 flex-col'>
       <ConferenceCard name={dictionary.conference.keynote_day1.speaker}
                       date={dictionary.conference.keynote_day1.date} image={'/conference/keynote_day1.png'}>
-        {lang === 'ja' ? <Day1Ja/> : <Day1En/>}
+        <Keynote1/>
       </ConferenceCard>
 
       <ConferenceCard name={dictionary.conference.keynote_day2.speaker}
                       date={dictionary.conference.keynote_day2.date} image={'/conference/keynote_day2.png'}>
-        {lang === 'ja' ? <Day2Ja/> : <Day2En/>}
+        <Keynote2/>
       </ConferenceCard>
     </div>
     <div className='w-full flex items-center flex-col my-10 gap-2.5'>
@@ -44,7 +49,7 @@ export default async function ConferenceSection({lang}: { lang: 'ja' | 'en' }) {
     <div className='flex lg:gap-7 gap-8 flex-col'>
       <ConferenceCard name={dictionary.conference.invited_talk.speaker}
                       date={dictionary.conference.invited_talk.date} image={'/conference/invitation_talk.png'}>
-        {lang === 'ja' ? <InvitationTalkJa/> : <InvitationTalkEn/>}
+        <InvitationTalk/>
       </ConferenceCard>
     </div>
   </section>
