b16e56482e34de8572938b6aca630cd2c9b30313
k-nakamura <koya.nakamura@gmail.com>
2024-09-08 18:29:39 +0900

    set runtime

--- src/app/[lang]/posters/page.tsx
+++ src/app/[lang]/posters/page.tsx
@@ -2,6 +2,9 @@ import {getDictionary} from "@/lib/dictionaries";
 import PageTitle from "@/components/elements/PageTitle";
 import PosterSection from "@/components/sections/PosterSection";
 
+export const runtime = 'edge';
+export const revalidate = 3600;
+
 export default async function Posters({params: {lang}}: { params: { lang: 'ja' | 'en' } }) {
   const dictionary = await getDictionary(lang);
 
