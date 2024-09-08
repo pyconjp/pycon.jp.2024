125a0fd35cbdfd3153b29aaec3d1aab188d0bdbb
k-nakamura <koya.nakamura@gmail.com>
2024-09-08 18:58:13 +0900

    add sprint link

--- src/app/[lang]/sprint/page.tsx
+++ src/app/[lang]/sprint/page.tsx
@@ -2,6 +2,8 @@ import {getDictionary} from "@/lib/dictionaries";
 import PageTitle from "@/components/elements/PageTitle";
 import SprintVenueSection from "@/components/sections/SprintVenueSection";
 import SprintSection from "@/components/sections/SprintSection";
+import AnnounceSection from "@/components/sections/AnnounceSection";
+import {sprintAnnounce} from "@/data/announce";
 
 export const runtime = 'edge';
 export const revalidate = 3600;
@@ -11,6 +13,7 @@ export default async function Sprint({params: {lang}}: { params: { lang: 'ja' | 
 
   return <main>
     <PageTitle title={'Sprint'} subTitle={dictionary.menu.sprint}/>
+    <AnnounceSection announce={sprintAnnounce} lang={lang}/>
     <SprintVenueSection lang={lang}/>
     <SprintSection/>
   </main>
--- src/components/elements/VenueCard.tsx
+++ src/components/elements/VenueCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function VenueCard({type, heading, venue}: {
   type: "conference" | "sprint",
   heading: { location: string, address: string, access: string },
-  venue: { location: string, address: string, access: string }
+  venue: { location: string, address: string, access: string, date: string }
 }) {
   const keys = ['location', 'address', 'access'] as const;
 
@@ -19,9 +19,14 @@ export default function VenueCard({type, heading, venue}: {
       (className, index) => <div key={index} className={'lg:h-11 lg:w-11 h-7 w-7 border-solid absolute ' + className}/>
     )}
     <div className='mx-auto my-auto object-contain h-full w-full flex lg:px-8 py-10 flex-col gap-6'>
-      <h3 className="w-full text-center font-bold font-manrope text-[2rem] leading-10">
-        {type === "conference" ? "Conference" : "Sprint"}
-      </h3>
+      <div className='w-full text-center'>
+        <h3 className="font-bold font-manrope text-[2rem] leading-10">
+          {type === "conference" ? "Conference" : "Sprint"}
+        </h3>
+        <h4 className='font-bold text-primary-400 text-lg'>
+          {venue.date}
+        </h4>
+      </div>
       <div className='flex lg:flex-row flex-col gap-7'>
         <div className="lg:flex-[2_2_0%] lg:aspect-auto aspect-[2_/_1]">
           <GoogleMap url={googleMapUrl[type]}/>
