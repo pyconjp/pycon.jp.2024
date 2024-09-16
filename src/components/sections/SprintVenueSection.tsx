248d4e1e1725998f6ae1681a4b4569f716fb25b6
k-nakamura <koya.nakamura@gmail.com>
2024-09-17 01:15:13 +0900

    Add Venue Page

--- src/components/sections/SprintVenueSection.tsx
+++ src/components/sections/SprintVenueSection.tsx
@@ -4,7 +4,7 @@ import VenueCard from "@/components/elements/VenueCard";
 export default async function SprintVenueSection({lang}: { lang: 'ja' | 'en' }) {
   const dictionary = await getDictionary(lang);
 
-  return <section className='lg:mt-28 mt-20'>
+  return <section className='lg:mt-28 mt-20 lg:w-10/12 w-11/12 mx-auto'>
     <VenueCard type={'sprint'} heading={dictionary.venue.heading} venue={dictionary.venue.venue.sprint}/>
   </section>
 }
--- src/data/menu.ts
+++ src/data/menu.ts
@@ -16,7 +16,7 @@ export const menu: Menu[] = [
     title: "about",
     children: [
       {title: "coc", url: "/coc", isComingSoon: false},
-      {title: "venue", url: "/venue", isComingSoon: true},
+      {title: "venue", url: "/venue", isComingSoon: false},
     ],
   },
   {
