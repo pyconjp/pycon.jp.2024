14a7c15c4510de415f46851b3434c089366c897d
k-nakamura <koya.nakamura@gmail.com>
2024-09-29 12:47:05 +0900

    Add Sprint Slide

---
+++ src/components/elements/GoogleSlide.tsx
@@ -0,0 +1,13 @@
+import {IframeHTMLAttributes} from "react";
+
+export default function GoogleSlide({slideId, ...props}: {
+  slideId: string,
+  props?: IframeHTMLAttributes<HTMLIFrameElement>
+}) {
+  return <iframe
+    src={`https://docs.google.com/presentation/d/${slideId}/embed?start=false&loop=false&delayms=60000`}
+    width="960" height="569" allowFullScreen
+    className={'w-full'}
+    {...props}
+  />
+}
